541418e2ad38961ca2a43f93ca6abb1b2c519395
SrKun13 <91975567+SrKun13@users.noreply.github.com>
2023-05-08 20:01:41 +0200

    t

---
+++ php/db/connectDB.php
@@ -0,0 +1,32 @@
+<?php 
+include("controlDB.php");
+class ConnectDB {
+
+    private $host = HOST;
+    private $dbusername = DBUSERNAME;
+    private $dbpassword = DBPASSWORD;
+    private $dbname = DBNAME;
+    public $db;
+
+    public function __construct() {
+        $this -> connectON();
+    }
+
+    private function connectON() {
+        try {
+            $this->db = new PDO("sqlsrv:Server=$this->host;database=$this->dbname", $this->dbusername, $this->dbpassword);
+            $this -> db -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        } catch (Exception $error) {
+            echo "No se ha podido conectar a la bd: ". $error -> getMessage();
+        }
+    }
+
+    public function getPDODB(): PDO {
+        return $this->db; // $this->conexionDB
+    }
+}
+
+
+?>
+
---
+++ php/db/controlDB.php
@@ -0,0 +1,5 @@
+<?php
+define('HOST', '172.17.0.1,1433');
+define('DBNAME', "Geolocation");
+define('DBUSERNAME', "SA");
+define('DBPASSWORD', "Lenovo.13-");
