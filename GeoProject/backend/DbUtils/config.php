bd39dc0b9880734670df01a4437150fd328848bf
Dragos Andrei <91976676+Dreico13@users.noreply.github.com>
2023-06-05 18:21:04 +0200

    Update config.php

--- GeoProject/backend/DbUtils/config.php
+++ GeoProject/backend/DbUtils/config.php
@@ -1,8 +1,8 @@
 <?php
 
-define('SERVERHOST','172.17.0.1,14333');
-define('SQLDB', 'GeoProject');
-define('USER','SA');
-define('PASS','@Asix13021997');
+define('SERVERHOST','');
+define('SQLDB', '');
+define('USER','');
+define('PASS','');
 
 ?>
